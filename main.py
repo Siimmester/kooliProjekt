8217ca6aab0fd60c09436051cb19bc97a00dd462
Siimmester <siimmar06@gmail.com>
2023-05-09 22:09:23 +0300

    algus

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+import math
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1910, 1070))
 clock = pygame.time.Clock()
 running = True
 
 hero_image = pygame.image.load("C:/kooliProjekt/Untitled.png")
 
-hero_x = 500
-hero_y = 300
+hero_x = 1800
+hero_y = 1000
+acceleration = 1
 
 while running:
     # poll for events
@@ -26,15 +28,20 @@ while running:
 
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP:
-            hero_y -= 1
-        elif event.key == pygame.K_DOWN:
-            hero_y += 1
+            acceleration += 150
         elif event.key == pygame.K_LEFT:
             hero_x -= 1
         elif event.key == pygame.K_RIGHT:
             hero_x += 1
 
+    if event.type == pygame.KEYUP:
+        if acceleration < 1:
+            acceleration = 1
+        elif event.key == pygame.K_UP and acceleration != 1:
+            acceleration = acceleration / 1.5
 
+    movement = math.log(acceleration, 6)
+    hero_y -= movement
     # blit the hero image onto the screen
     screen.blit(hero_image, (hero_x, hero_y ))
 
