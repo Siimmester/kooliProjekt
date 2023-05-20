75f9b65d31d1cc0ed321639f5e98b04b93498aa4
Siimmester <siimmar06@gmail.com>
2023-05-20 19:22:57 +0300

    põõramine

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ hero_image = pygame.image.load("C:/kooliProjekt/Untitled.png")
 hero_x = 1800
 hero_y = 1000
 acceleration = 1
-
+time_helddown = 0
+rotation = 0
+var1 = 1
+var2 = 0
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -26,21 +29,70 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        acceleration += 150
-    if keys[pygame.K_RIGHT]:
-        hero_x += 1
-    if keys[pygame.K_LEFT]:
-        hero_x -= 1
+        if time_helddown < 1:
+            time_helddown += 0.005
+        acceleration += 150 * time_helddown
 
+    if keys[pygame.K_RIGHT]:
+        if rotation >= 360:
+            rotation -= 360
+        elif rotation <= 0:
+            rotation += 360
+        rotation -= 1
+    if keys[pygame.K_LEFT]:
+        if rotation <= 0:
+            rotation += 360
+        elif rotation >= 360:
+            rotation -= 360
+        rotation += 1
 
     if event.type == pygame.KEYUP:
         if acceleration < 1:
             acceleration = 1
-        elif event.key == pygame.K_UP and acceleration != 1:
-            acceleration = acceleration / 1.1
+        elif event.key != pygame.K_DOWN and acceleration != 1:
+            acceleration = acceleration / 1.6
 
     movement = math.log(acceleration, 4)
-    hero_y -= movement
+
+    if 90 >= rotation >= 0:
+        if rotation != 0:
+            var1 = rotation / 90
+        else:
+            var1 = 0
+        var2 = movement * var1
+
+        var3 = movement - var2
+        hero_y -= var3
+        hero_x -= var2
+
+    if 180 >= rotation > 90:
+        rotation1 = rotation - 90
+        var1 = rotation1 / 90
+        var2 = movement * var1
+        var3 = movement - var2
+
+        hero_y += var2
+        hero_x -= var3
+
+    if 270 >= rotation > 180:
+        rotation1 = rotation - 180
+        var1 = rotation1 / 90
+        var2 = movement * var1
+        var3 = movement - var2
+
+        hero_y += var3
+        hero_x += var2
+
+    if 360 >= rotation > 270:
+        rotation1 = rotation - 270
+        var1 = rotation1 / 90
+        var2 = movement * var1
+        var3 = movement - var2
+
+        hero_y -= var2
+        hero_x += var3
+
+    print(var2, var1, rotation, movement)
 
     if hero_y < -100:
         hero_y = 1200
@@ -53,7 +105,6 @@ while running:
 
     if hero_x > 2000:
         hero_x = -100
-
 
     # blit the hero image onto the screen
     screen.blit(hero_image, (hero_x, hero_y))
